ab89c304234257528494d85fe2f35e3d80fee82c
itheima <hello@itheima.com>
2022-05-18 16:03:15 +0800

    添加部分代码注释

--- huike-task/src/main/java/com/huike/task/mapper/RecoveryMapper.java
+++ huike-task/src/main/java/com/huike/task/mapper/RecoveryMapper.java
@@ -6,11 +6,11 @@ import org.apache.ibatis.annotations.Update;
 import java.util.Date;
 
 public interface RecoveryMapper {
-    //线索回收
+    //线索回收：线索失效时间 <= 当前时间，线索可能已经无效，系统自动回收到线索池
     @Update("update tb_clue set next_time=null, status=#{status} where end_time <= #{now} and status IN (1,2,4)")
     void resetNextTimeAndStatusOnClue(@Param("status")String status, @Param("now") Date date);
 
-    //商机回收
+    //商机回收：商机失效时间 <= 当前时间，商机可能已经无效，系统自动回收到公海池
     @Update("update tb_business set next_time=null, status=#{status} where end_time <= #{now} and status IN (1,2,4)")
     void resetNextTimeAndStatusOnBusiness(@Param("status")String tbBusiness_recovery,@Param("now") Date date);
 
